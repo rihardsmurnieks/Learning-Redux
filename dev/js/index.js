e367f332bfd71c34a72b513e543c092c99c88f5d
Rihards Murnieks <rihardsmurnieks@Rihardss-iMac.local>
2017-10-08 18:02:28 +0300

    First commit

---
+++ dev/js/index.js
@@ -0,0 +1,10 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from "react-dom";
+import {createStore} from 'redux'; // curly braces allow us to import just one function or
+// variable from a module
+
+
+
+
+ReactDOM.render(<h1>Hey now</h1>, document.getElementById('root'));
